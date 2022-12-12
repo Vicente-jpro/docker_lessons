d1d226f92ac174e1c1eaaf9240edbc6b4ed60285
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-12-12 02:05:40 +0100

    Choosing a port  my_computer_port:docker_port

--- comands.sh
+++ comands.sh
@@ -23,3 +23,7 @@ docker container ls -a
 
 #Stop a container
 docker stop <name>
+
+#Choosing a port  my_computer_port:docker_port. Ex: 3000:80
+#My browser going to understand localhost:3000
+docker run -p 3000:80 
