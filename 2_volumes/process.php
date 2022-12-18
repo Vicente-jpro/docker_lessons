16f0cd39060a2fcfea52590bae8893c4a08434e1
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-12-18 14:18:37 +0100

    port changed

--- 2_volumes/process.php
+++ 2_volumes/process.php
@@ -2,14 +2,14 @@
 
     $message = $_POST["message"];
 
-    $files = scandir('./messages');
+    $files = scandir("./messages");
     $num_files = count($files) - 2;
 
 
     $fileName = "msg-{$num_files}.txt";
-    $file = fopen("./messages/{$fileName}", x);
+    $file = fopen("./messages/{$fileName}", "x");
 
-    $fwrite($file, $message);
-    $fclose($file);
+    fwrite($file, $message);
+    fclose($file);
     header("Location: index.php");
 ?>
