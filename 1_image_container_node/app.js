d21b25c9e806729d80eaa62b6109f8b398d109a0
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-12-18 09:35:15 +0100

    message modified

--- 1_image_container_node/app.js
+++ 1_image_container_node/app.js
@@ -3,7 +3,7 @@ const app = express()
 const port = 3000
 
 app.get('/', (req, res) => {
-    res.send('Olá minha mensagem')
+    res.send('Olá minha mensagem version')
 })
 
 app.listen(port, () => {
