a4313eb76a1965e4e68d212eade6e30bca90d875
Vicente-jpro <70174120+Vicente-jpro@users.noreply.github.com>
2022-12-23 12:14:16 +0100

    Python added

---
+++ 3_networks/app.py
@@ -0,0 +1,19 @@
+import flask
+from flask import request, json, jsonify
+import request
+
+app = flask.Flask(__name__)
+app.config["DEBUG"] = True
+
+@app.route("/", methods=["GET"])
+
+def index():
+    data = requests.get('https://randomuser.me/api')
+    return data.json()
+
+if __name__ == "__main__":
+    app.run(
+        host = "0.0.0.0", 
+        debug = True,
+        port = "5000"
+    )
